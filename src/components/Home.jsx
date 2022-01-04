7c249decafa62b8551d487885d93e0c2eb56912b
IPbiramontes <isabiramontes@gmail.com>
2022-01-04 18:33:59 -0500

    css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,24 +7,32 @@ function Home() {
         <div className="home">
             <div className="scrollContent" style={{ width: "100vw", overflow: "hidden" }}>
                 <div className="parallax" id="introSection">
-                    <h1 className="fullName"> Isa
+                    <h1 className="fullName"> ISA
                         <span id="twentyText" style={{ fontSize: "15px" }}>2020</span>
                         <span className="boldHeader" style={{ display: "block", paddingTop: "12vw" }}> Biramontes </span>
                     </h1>
                     <div id="home_pic"> </div>
                 </div>
-                <div className="parallax" id="aboutSection" style={{ width: "100vw", overflow: "hidden" }}>
+                <div className="parallax" id="aboutSection" style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
                     <Container>
+                        <Row></Row>
                         <Row>
-                            <Col></Col>
+                            <Col>
+                            <p> test</p>
+                            
+                            </Col>
                             <Col className="ddaText"> 
                                 <h1 className="boldHeader"> Developer,
                                     <span style={{ display: "block" }}>Designer,</span>
-                                    <span style={{ display: "block" }}> Artist. </span>
+                                    <span style={{ display: "block" }}>Artist. </span>
                                 </h1>
                             </Col>
                         </Row>
+                        <Row></Row>
                     </Container>
+                </div>
+                <div className="parallax" id="navSection" style={{ height: "100vh" }}>
+                    <h1> test </h1>
                 </div>    
             </div>  
         </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,20 +5,20 @@ import "../App.css"
 
 function Navigation() {
     return (
-        <div className="navigation">
-            <Container>
-                <Nav className="d-flex justify-content-between" activeKey="/home" sticky="top">
+        <div className="navigation" style={{ width: "100vw" }}>
+            <Container className="navContainer">
+                <Nav className="d-flex justify-content-between" activeKey="/home">
                     <Nav.Item>
-                        <NavLink className="nav-link" to="/home" style={{ color: "#1c1c21" }}> Home </NavLink>
+                        <NavLink className="nav-link" to="/home" style={{ color: "#1c1c21" }}> HOME </NavLink>
                     </Nav.Item>
                     <Nav.Item>
-                        <NavLink className="nav-link" to="/assignments" style={{ color: "#1c1c21" }}> Assignments </NavLink>
+                        <NavLink className="nav-link" to="/assignments" style={{ color: "#1c1c21" }}> ASSIGNMENTS </NavLink>
                     </Nav.Item>
                     <Nav.Item>
-                        <NavLink className="nav-link" to="/final" style={{ color: "#1c1c21" }}> Final </NavLink>
+                        <NavLink className="nav-link" to="/final" style={{ color: "#1c1c21" }}> FINAL </NavLink>
                     </Nav.Item>
                     <Nav.Item>
-                        <NavLink className="nav-link" to="/about" style={{ color: "#1c1c21" }}> About </NavLink>
+                        <NavLink className="nav-link" to="/home#aboutSection" style={{ color: "#1c1c21" }}> ABOUT </NavLink>
                     </Nav.Item>
                 </Nav>
             </Container>
