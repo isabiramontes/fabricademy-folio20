6801be8ca6a3f4bc8f60591059e7d43789227540
IPbiramontes <isabiramontes@gmail.com>
2022-01-04 17:39:27 -0500

    home page updated. need 3 disable hor scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,32 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import "../App.css"
+import "../App.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
     return (
         <div className="home">
-            <p>test</p>
-            <div className="scrollContent">
+            <div className="scrollContent" style={{ width: "100vw", overflow: "hidden" }}>
                 <div className="parallax" id="introSection">
                     <h1 className="fullName"> Isa
-                        <span id="twentyText" style="font-size: 15px;">2020</span>
-                        <span className="boldHeader" style="display: block; padding-top: 12vw;"> Biramontes </span>
+                        <span id="twentyText" style={{ fontSize: "15px" }}>2020</span>
+                        <span className="boldHeader" style={{ display: "block", paddingTop: "12vw" }}> Biramontes </span>
                     </h1>
                     <div id="home_pic"> </div>
                 </div>
-                <div className="parallax" id="aboutSection">
-                    <div className="ddaText">
-                        <h1 className="boldHeader"> Developer,
-                            <span style="display: block;">Designer,</span>
-                            <span style="display: block;"> Artist. </span>
-                        </h1>
-                    </div>
-                </div>
-            </div>
+                <div className="parallax" id="aboutSection" style={{ width: "100vw", overflow: "hidden" }}>
+                    <Container>
+                        <Row>
+                            <Col></Col>
+                            <Col className="ddaText"> 
+                                <h1 className="boldHeader"> Developer,
+                                    <span style={{ display: "block" }}>Designer,</span>
+                                    <span style={{ display: "block" }}> Artist. </span>
+                                </h1>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>    
+            </div>  
         </div>
     );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,18 +7,18 @@ function Navigation() {
     return (
         <div className="navigation">
             <Container>
-                <Nav className="d-flex justify-content-between" activeKey="/home">
+                <Nav className="d-flex justify-content-between" activeKey="/home" sticky="top">
                     <Nav.Item>
-                        <NavLink className="nav-link" to="/home"> Home </NavLink>
+                        <NavLink className="nav-link" to="/home" style={{ color: "#1c1c21" }}> Home </NavLink>
                     </Nav.Item>
                     <Nav.Item>
-                        <NavLink className="nav-link" to="/assignments"> Assignments </NavLink>
+                        <NavLink className="nav-link" to="/assignments" style={{ color: "#1c1c21" }}> Assignments </NavLink>
                     </Nav.Item>
                     <Nav.Item>
-                        <NavLink className="nav-link" to="/final"> Final </NavLink>
+                        <NavLink className="nav-link" to="/final" style={{ color: "#1c1c21" }}> Final </NavLink>
                     </Nav.Item>
                     <Nav.Item>
-                        <NavLink className="nav-link" to="/about"> About </NavLink>
+                        <NavLink className="nav-link" to="/about" style={{ color: "#1c1c21" }}> About </NavLink>
                     </Nav.Item>
                 </Nav>
             </Container>
