368b62140673bafe4835164b8bf204b644000833
IPbiramontes <isabiramontes@gmail.com>
2022-01-19 15:45:33 -0500

    menu foundation completed. adding content

--- src/components/WeekOne.jsx
+++ src/components/WeekOne.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import "../App.css"
 import "../weeks.css"
-import { Container } from "react-bootstrap";
+import { Container, Row, Col} from "react-bootstrap";
 
 function WeekOne() {
     let { weekSlug } = useParams();
@@ -13,27 +13,34 @@ function WeekOne() {
 
     return (
         <div className="home">
-            <Container>
-                <h1 className="boldHeader"> State of the art, project management & documentation </h1>
-                <h1 className="mt-5"> Week 1</h1>
-                <h6 className="mb-5">The post slug is, {weekSlug}</h6>
-                <p>
-                    This is WEEK ONE talking
-                </p>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text ever
-                    since the 1500s, when an unknown printer took a galley of type and
-                    scrambled it to make a type specimen book.
-                </p>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy text ever
-                    since the 1500s, when an unknown printer took a galley of type and
-                    scrambled it to make a type specimen book.
-                </p>
-
-            </Container>
+            <div className="scrollContent" style={{ width: "100vw", overflow: "hidden" }}>
+                <div className="parallax" style={{ backgroundColor:"#e9c149", paddingTop: "18vh"}}>
+                    <Container>
+                        <h1 className="weekNameandweekNum"> State of the art, project management & documentation </h1>
+                        <h1 className="weekIdentity"> Week 1</h1>
+                        <p className="weekHookText">This week I worked on start to brainstorm for my final project and learning/adjusting to the documentation process. {weekSlug}</p>
+                    </Container>
+                </div>
+                <div className="parallax" style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
+                    <Container>
+                        <Row style={{ paddingTop: "20vh" }}>
+                            <Col sm={7} md={7} lg={7}>
+                                <h1> Developer,
+                                    <span style={{ display: "block" }}>Designer,</span>
+                                    <span style={{ display: "block" }}>Artist. </span>
+                                </h1>
+                            </Col>
+                            <Col sm={5} md={5} lg={5}>
+                                <div>
+                                    <h1 className="bodyText" style={{ fontSize: "1.7em", fontWeight: "bold", textAlign: "right" }}>Lorem ipsum</h1>
+                                    <p className="defaultTextBody" style={{ paddingLeft: "10%", textAlign:"right" }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu turpis egestas pretium aenean. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Dolor magna eget est lorem. Convallis posuere morbi leo urna. In mollis nunc sed id semper risus in. Congue eu consequat ac felis donec et odio. Purus faucibus ornare suspendisse sed nisi. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim.  </p>
+                                </div>
+                            </Col>
+                            
+                        </Row>
+                        </Container>
+                </div>
+            </div>  
         </div>
     );
 }
--- src/components/Weeks.jsx
+++ src/components/Weeks.jsx
@@ -6,20 +6,37 @@ import "../weeks.css"
 function Weeks() {
     return (
         <div className="weeks">
-            <div className="parallax" style={{ height: "50vh", width: "100vw" }}>
-                <Container style={{ marginLeft: "0", marginRight: "0", }}>
+            <div className="parallax">
+                <Container style={{ marginLeft: "0", marginRight: "0"}}>
                     <Row className="menuRow">
                         <Col sm={2} md={2} lg={2} className="weekCol">
                             <h1 className="weekNum"> 01 </h1>
                         </Col>
-                        <Col sm={4} md={4} lg={4} className="imgCol">
+                        <Col sm={3} md={3} lg={3} className="imgCol">
                             <div className="imgBox" />
                         </Col>
-                        <Col sm={5} md={5} lg={5} className="weekInfoCol">
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
                             <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
                                 <h1 className="weekName">state of the art, project management & documentation</h1>
                             </Link>
-                            <p className="defaultTextBody" >This week I worked on start to brainstorm for my final project and learning/adjusting to the documentation process.</p>
+                            <p className="defaultTextBody" >Brainstorming for my final project and learning/adjusting to the documentation process.</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                    <Row className="menuRow" >
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 02 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Digital Bodies</h1>
+                            </Link>
+                            <p className="defaultTextBody" >Learning how to create 3D models using MakeHuman / Rhinoceros and how to translate those models into real life models using Slicer for Fusion 360.</p>
                         </Col>
                         <Col sm={1} md={1} lg={1} className="arrowCol">
                             <h1 className="weekNum"> k </h1>
@@ -27,22 +44,214 @@ function Weeks() {
                     </Row>
                     <Row className="menuRow">
                         <Col sm={2} md={2} lg={2} className="weekCol">
-                            <h1 className="weekNum"> 02 </h1>
+                            <h1 className="weekNum"> 03 </h1>
                         </Col>
-                        <Col sm={4} md={4} lg={4} className="imgCol">
+                        <Col sm={3} md={3} lg={3} className="imgCol">
                             <div className="imgBox" />
                         </Col>
-                        <Col sm={5} md={5} lg={5} className="weekInfoCol">
-                            <Link to="/Projects/WeekOne"style={{ color: "#0b0b0a" }}>
-                                <h1 className="weekName">state of the art, project management & documentation</h1>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Circular fashion</h1>
                             </Link>
-                            <p className="defaultTextBody" >This week I worked on start to brainstorm for my final project and learning/adjusting to the documentation process.</p>
+                            <p className="defaultTextBody">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row> 
+                </Container>
+            </div>
+            <div className="parallax" style={{ backgroundColor: "pink", paddingTop:"6vh"}}>
+                <Container style={{ marginLeft: "0", marginRight: "0" }}>
+                    <Row className="menuRow">
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 04 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">biochromes</h1>
+                            </Link>
+                            <p className="defaultTextBody" >Learning about Biochromes, a more natural sustainable approach to pigments and textile dying, to develop a natural dye and ink of my own.</p>
                         </Col>
                         <Col sm={1} md={1} lg={1} className="arrowCol">
                             <h1 className="weekNum"> k </h1>
                         </Col>
                     </Row>
-                    
+                    <Row className="menuRow" >
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 05 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">E-textiles</h1>
+                            </Link>
+                            <p className="defaultTextBody">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                    <Row className="menuRow">
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 06 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">BioFabricating Materials</h1>
+                            </Link>
+                            <p className="defaultTextBody"> Exploring available technology to develop bio-based materials as an alternative to current, less sustainable options. </p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <div className="parallax" style={{ paddingTop: "6vh" }}>
+                <Container style={{ marginLeft: "0", marginRight: "0" }}>
+                    <Row className="menuRow">
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 07 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Computational Couture</h1>
+                            </Link>
+                            <p className="defaultTextBody">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                    <Row className="menuRow" >
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 08 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Open Source Hardware - From Fibers to Fabric</h1>
+                            </Link>
+                            <p className="defaultTextBody">For our first and only fully collaborative week, myself, Noémie Carrier, Annie Ferlatte, and François Auclair built our own automated loom.</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                    <Row className="menuRow">
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 09 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Textile as scaffold</h1>
+                            </Link>
+                            <p className="defaultTextBody">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <div className="parallax" style={{ paddingTop: "6vh", backgroundColor:"pink" }}>
+                <Container style={{ marginLeft: "0", marginRight: "0" }}>
+                    <Row className="menuRow">
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 10 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Wearables</h1>
+                            </Link>
+                            <p className="defaultTextBody">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                    <Row className="menuRow" >
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 11 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Implications and applications</h1>
+                            </Link>
+                            <p className="defaultTextBody">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                    <Row className="menuRow">
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 12 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Soft robotics</h1>
+                            </Link>
+                            <p className="defaultTextBody">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <div className="parallax" style={{ paddingTop: "6vh" }}>
+                <Container style={{ marginLeft: "0", marginRight: "0" }}>
+                    <Row className="menuRow">
+                        <Col sm={2} md={2} lg={2} className="weekCol">
+                            <h1 className="weekNum"> 13 </h1>
+                        </Col>
+                        <Col sm={3} md={3} lg={3} className="imgCol">
+                            <div className="imgBox" />
+                        </Col>
+                        <Col sm={6} md={6} lg={6} className="weekInfoCol">
+                            <Link to="/Projects/WeekOne" style={{ color: "#0b0b0a" }}>
+                                <h1 className="weekName">Skin Electronics</h1>
+                            </Link>
+                            <p className="defaultTextBody">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+                        </Col>
+                        <Col sm={1} md={1} lg={1} className="arrowCol">
+                            <h1 className="weekNum"> k </h1>
+                        </Col>
+                    </Row>
+                    <Row className="finalProjectRow" >
+                        <Col sm={12} md={12} lg={12}>
+                            <h1>Final Project</h1>
+                            <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+                            
+                        </Col>
+                    </Row>
                 </Container>
             </div>
         </div>
