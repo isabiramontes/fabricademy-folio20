4fbee995072195b9cdf08a06324143ad85d7625b
IPbiramontes <isabiramontes@gmail.com>
2022-01-20 14:02:10 -0500

    adding and testing a new file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import { Navigation, Home, About, Final, Projects, Weeks, WeekOne } from './components';
+import { Navigation, Home, About, Final, Projects, Week} from './components';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,10 +15,8 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />} />
           <Route path="/final" element={<Final />} />
-          <Route path="/projects" element={<Projects />}>
-              <Route path="" element={<Weeks />} />
-              <Route path=":weeksSlug" element={<WeekOne />} />
-          </Route> 
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/weektest" element={<WeekTest />} />
         </Routes>
       </Router>
     </div>
